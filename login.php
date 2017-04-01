f49e021f9c1bdbf287cadef7f0794f562fdcef01
Dan Herlihy <djherls@gmail.com>
2017-03-31 21:29:20 -0400

    Created logic to update timestamp

--- login.php
+++ login.php
@@ -1,39 +1,49 @@
 <?php
 
 $settings = parse_ini_file("passwords.txt");
-$conn = new mysqli("localhost", $settings['username'], $settings['password']);
+$conn = new mysqli("localhost", $settings['username'], $settings['password'], $settings['database']);
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    die();
+}
 
 if (!$_POST['username']){
 	header("Location: https://webaccess.psuits.org");
 	die();
 }
-
-if (!preg_match('/[a-zA-Z0-9]{3,7}',$_POST['username']){
+if (!preg_match('/^[a-zA-Z0-9]{3,7}$/',$_POST['username'])){
 	http_response_code(403);
 	die();
 }
-
 // Check connection
-if ($conn) {
-    if ($stmt = $mysqli->prepare("SELECT * FROM victims WHERE access_id=?")) {
+if ($stmt = $conn->prepare("SELECT login_at FROM victims WHERE access_id=?")) {
+	/* bind parameters for markers */
+	$stmt->bind_param("s", $_POST['username']);
 
-		/* bind parameters for markers */
-		$stmt->bind_param("s", $_POST['username']);
+	/* execute query */
+	$stmt->execute();
 
-		/* execute query */
-		$stmt->execute();
+	/* bind result variables */
+	$stmt->bind_result($time);
 
-		/* bind result variables */
-		$stmt->bind_result($victim);
+	/* fetch value */
+	$stmt->fetch();
 
-		/* fetch value */
-		$stmt->fetch();
+	$stmt->close();
 
-		print_r($victim);
-
-		/* close statement */
-    $stmt->close();
+	if (!$time){
+		if ($insert = $conn->prepare("UPDATE victims SET login_at=? WHERE access_id=?")){
+			$insert->bind_param("ss", date("Y-m-d H:i:s"), $_POST['username']);
+			$insert->execute();
+			$insert->close();
+		}
+		
 	}
+
+	/* close statement */
 }
+
 
 $conn->close();
